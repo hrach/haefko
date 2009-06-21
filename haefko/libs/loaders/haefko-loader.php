65168c8357af7c5c494984e7fb1b9f44ec9c3f64
Jan Škrášek <hrach.cz@gmail.com>
2009-06-21 18:49:59 +0000

    - added new system of loaders

---
+++ haefko/libs/loaders/haefko-loader.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once dirname(__FILE__) . '/loader.php';
+require_once dirname(__FILE__) . '/../tools.php';
+
+
+
+class HaefkoLoader extends Loader
+{
+
+	
+	/** @var array - Available framework classes */
+	protected $classes = array(
+		# application
+		'application'  => 'application.php',
+		'inflector'    => 'application/libs/inflector.php',
+		'router'       => 'application/libs/router.php',
+		# libs
+		'cache'        => 'libs/cache.php',
+		'config'       => 'libs/config.php',
+		'cookie'       => 'libs/cookie.php',
+		'datagird'     => 'libs/datagrid.php',
+		'debug'        => 'libs/debug.php',
+		'object'       => 'libs/object.php',
+		'form'         => 'libs/form.php',
+		'html'         => 'libs/html.php',
+		'http'         => 'libs/http.php',
+		'l10n'         => 'libs/l10n.php',
+		'paginator'    => 'libs/paginator.php',
+		'session'      => 'libs/session.php',
+		'template'     => 'libs/template.php',
+		'tools'        => 'libs/tools.php',
+		'user'         => 'libs/user.php',
+		# database
+		'db'           => 'libs/db.php',
+		'dbstructure'  => 'libs/db-structure.php',
+		'dbtable'      => 'libs/db-table.php',		
+		# loaders
+		'autoloader'   => 'libs/loaders/auto-loader.php',
+	);
+
+
+	/**
+	 * Loads file for required class
+	 * @param   string    class name
+	 * @return  void
+	 */
+	public function load($class)
+	{
+		$c = strtolower($class);
+		if (Tools::endWith($c, 'controller'))
+			return Application::get()->loadClass('controller', $class);
+		elseif (Tools::endWith($c, 'helper'))
+			return Application::get()->loadClass('helper', $class);
+
+		if (isset($this->classes[$c]))
+			require_once dirname(__FILE__) . '/../../' . $this->classes[$c];
+	}
+
+
+	/**
+	 * Registers haefko loader
+	 * @return  void
+	 */
+	public function register()
+	{
+		parent::register(array($this, 'load'));
+	}
+
+
+}
---
+++ haefko/libs/loaders/loader.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Haefko - your php5 framework
+ *
+ * @author      Jan Skrasek
+ * @copyright   Copyright (c) 2007 - 2009, Jan Skrasek
+ * @link        http://haefko.skrasek.com
+ * @license     http://www.opensource.org/licenses/mit-license.html
+ * @version     0.8.5 - $Id$
+ * @package     Haefko_Libs
+ */
+
+
+require_once dirname(__FILE__) . '/../object.php';
+
+
+/**
+ * 
+ */
+abstract class Loader extends Object
+{
+
+
+	/** @var array */
+	private $callbacks = array();
+
+
+	/**
+	 * Register callback for loader hanledr
+	 * @param   mixed  $callback
+	 * @return  void
+	 */
+	public function register($callback)
+	{
+		if (!is_callable($callback))
+			throw new Exception('Loader callback is not callable');
+
+		if (empty($this->callbacks))
+			spl_autoload_register(array($this, 'autoloadHandler'));
+
+		$this->callbacks[] = $callback;
+	}
+
+
+	/**
+	 * Autoload handler
+	 * @param   string    class name
+	 * @return  void
+	 */
+	public function autoloadHandler($class)
+	{
+		foreach ($this->callbacks as $cb) {
+			call_user_func($cb, $class);
+			if (class_exists($class, false))
+				break;
+		}
+	}
+
+
+}
