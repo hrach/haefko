9d3577b754e02af31688dd4ca5761e93fef7fcfa
Jan Škrášek <hrach.cz@gmail.com>
2009-07-19 19:38:20 +0000

    - updated and added examples

---
+++ examples/permission/index.php
@@ -0,0 +1,41 @@
+<?php
+
+require '../../haefko/loader.php';
+debug::init(true);
+
+
+$acl = new Permission();
+
+
+# roles
+$acl->addRole('member', 'guest');
+$acl->addRole('admin', 'member');
+$acl->addRole('superadmin', 'admin');
+
+
+# resource
+$acl->addResource('comments');
+$acl->addResource('posts');
+
+
+# privilegies
+$acl->allow('guest', array('posts', 'comments'), 'view');
+$acl->allow('member', 'comments', 'add');
+$acl->allow('admin', 'posts', array('add', 'edit', 'delete'));
+$acl->allow('superadmin', '*', '*');
+
+
+
+echo "<br>allowed: " . ($acl->isAllowed('guest', 'posts', 'view') ? "allowed" : "denied");
+echo "<br>allowed: " . ($acl->isAllowed('guest', 'comments', 'view') ? "allowed" : "denied");
+echo "<br>allowed: " . ($acl->isAllowed('member', 'comments', 'view') ? "allowed" : "denied");
+echo "<br>allowed: " . ($acl->isAllowed('admin', 'comments', 'add') ? "allowed" : "denied");
+echo "<br>allowed: " . ($acl->isAllowed('admin', 'posts', 'view') ? "allowed" : "denied");
+echo "<br>allowed: " . ($acl->isAllowed('superadmin', 'posts', 'delete') ? "allowed" : "denied");
+echo "<br>allowed: " . ($acl->isAllowed('superadmin', 'comments', 'delete') ? "allowed" : "denied");
+
+echo "<br>";
+echo "<br>denied: " . ($acl->isAllowed('guest', 'comments', 'add') ? "allowed" : "denied");
+echo "<br>denied: " . ($acl->isAllowed('guest', 'posts', 'add') ? "allowed" : "denied");
+echo "<br>denied: " . ($acl->isAllowed('member', 'comments', 'delete') ? "allowed" : "denied");
+echo "<br>denied: " . ($acl->isAllowed('admin', 'comments', 'delete') ? "allowed" : "denied");
---
+++ examples/template/template.tpl
@@ -0,0 +1,8 @@
+{extends 'layout.tpl'}
+
+{block append #content}
+<div style="border: 1px solid #999; background: #efefef; padding: 1em">
+	test
+	{include 'template.include.tpl'}
+</div>
+{/block}
