462c8ad0529aa14040f411def3bfa66553176910
Jan Škrášek <hrach.cz@gmail.com>
2009-08-06 14:57:31 +0000

    - application - added ability to set error controller - default is app controller - control - added missing class

---
+++ haefko/libs/control.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Haefko - your php5 framework
+ *
+ * @author      Jan Skrasek
+ * @copyright   Copyright (c) 2007 - 2009, Jan Skrasek
+ * @link        http://haefko.skrasek.com
+ * @license     http://www.opensource.org/licenses/mit-license.html
+ * @version     0.9 - $Id: controller.php 173 2009-07-25 19:24:02Z skrasek.jan $
+ * @package     Haefko
+ */
+
+
+class Control extends Object
+{
+
+	/** @var SessionNamespace */
+	private $flashSession;
+
+
+	/**
+	 * Contructor
+	 * @return Control
+	 */
+	public function __construct()
+	{
+		if (empty($this->flashSession))
+			$this->flashSession = Session::getNamespace('flash-messages.' . $this->getClass());
+	}
+
+
+	/**
+	 * Adds flash message and returns message class
+	 * @param string $message
+	 * @param string $type
+	 * @return stdClass
+	 */
+	public function addFlash($message, $type = 'info')
+	{
+		$messages = $this->flashSession->messages;
+		$messages[] = $flash = (object) array(
+			'message' => $message,
+			'type' => $type,
+		);
+
+		$this->flashSession->set('messages', $messages, time() + 3);
+		return $flash;
+	}
+
+
+	/**
+	 * Returns flash messages
+	 * @return array
+	 */
+	public function getFlashes()
+	{
+		$messages = $this->flashSession->messages;
+		if (empty($messages))
+			return array();
+		else
+			return $messages;
+	}
+
+
+	/**
+	 * Returns template instance
+	 * @return ITemplate
+	 */
+	protected function getTemplateInstace()
+	{
+		$template = new Template();
+		$template->flashes = $this->getFlashes();
+		return $template;
+	}
+
+
+}
